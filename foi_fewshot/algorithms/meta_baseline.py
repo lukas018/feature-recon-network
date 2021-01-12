af8680b50447c366572e5d6d692497cb56fb3dff
Lukas Lundmark <lukas018.lundmark@gmail.com>
2021-01-12 13:44:51 +0100

    Added caching option for centroids
    Added option to save centroids to reuse later

--- foi_fewshot/algorithms/meta_baseline.py
+++ foi_fewshot/algorithms/meta_baseline.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .frn import FeatureReconNetwork
-
 
 
 class MetaBaseline():
@@ -19,6 +17,8 @@ class MetaBaseline():
         self.model = model
         self.temperature = torch.tensor(temperature)
         self.dist_fn = dist_fn
+        self.class_matrix = None
+        self.cached_centriods
 
     def init_pretraining(self, dimensions: int, num_classes: int):
         """Initialize the FRN for pre-training
@@ -29,6 +29,27 @@ class MetaBaseline():
         if self.class_matrix is not None or self.class_matrix.shape[1] == num_classes:
             self.class_matrix = nn.Linear(dimensions, num_classes)
 
+
+    def compute_centroids(self, support, cache=False):
+        """Computes the centroids of the given support imgages
+
+
+        :param support: Tensor of size [n, k, h, w, c]
+            It is assumed that the images is grouped with regards to class the classes
+        :param cache: Set to true to cache the centriods to use for later fewshot learning
+
+        """
+
+        nway = support.shape[0]
+        k = support.shape[1]
+        features = features.unflatten(0).repeat((nway, 1, 1))
+        support_features = self.model(support.flatten(0, 1)).reshape((nway, k, -1))
+        centroids = support_features.mean(axis=1)
+
+        if cache:
+            self.cached_centroids = centroids
+
+        return centroids
 
     def forward(self,
                 query: nn.Torch,
@@ -46,7 +67,7 @@ class MetaBaseline():
         and is used to compute the class representation matrices used for prediction.
 
         :param query: Set of images such that shape=(bsz x h x w x channels)
-        :param support: Set of images such that shape=(n x k x h x w x channels)
+        :param support: Set of images such that shape=(bsz x k x h x w x channels)
 
         :return: Prediction for each imput image. Logit dimension depends on mode-used.
         """
@@ -56,25 +77,28 @@ class MetaBaseline():
         # flatten the input to bsz x dim_f
         features = self.model(query).flatten(1, 3)
 
-        if support is not None:
-            # Do few-shot prediction
-            nway = support.shape[0]
-            k = support.shape[1]
-            img = support.shape[2:]
+        if support is not None or self.cached_centroids is not None:
 
-            features = features.unflatten(0).repeat((nway, 1, 1))
+            if support is not None:
+                centroids = self.compute_centroids(support)
 
-            support_features = self.model(support.flatten(0, 1)).reshape((nway, k, -1))
-            centroids = support_features.mean(axis=1)
+            elif self.cached_centroids is not None:
+                centroids = self.cached_centroids
+
+            nway = self.cached_centroids
+            features = features.repeat((nway, 1, 1))
             centroids = centroids.unflatten(1).repeat((1, bsz, 1))
 
-            logits = self.dist_fn(features, support_features)
+            logits = self.dist_fn(features, centroids)
             logits = F.softmax(self.temperature*logits)
-            logits = (logits, 0)
 
         else:
+
             # Do a normal forward pass
             features = self.model(query)
+            if self.class_matrix is None:
+                raise ValueError(f"Final classification layer was not initialized, please run init_pretraining before calling")
+
             logits = self.class_matrix(features)
             logits = F.softmax(logits)
 
