164fa8b690e8ecb8656189990dab1cb3e5d85f5f
Lukas Lundmark <lukas.lundmark@foi.se>
2021-01-26 09:05:06 +0100

    Changed the example to be less cluttered

--- examples/mini_imagenet_test.py
+++ examples/mini_imagenet_test.py
@@ -10,6 +10,8 @@ from foi_fewshot.trainers import FewshotArguments
 from foi_fewshot.trainers import FewshotTrainer
 from foi_fewshot.trainers import PretrainArguments
 from foi_fewshot.trainers import PreTrainer
+from foi_fewshot.trainers import SchedulerUpdateStrategy
+
 
 # Initialize the network backcone
 model = ResNet12()
@@ -17,37 +19,42 @@ model = ResNet12()
 meta_baseline = MetaBaseline(model)
 
 # Initialize the datasets
-ds_train, ds_novel, ds_test = mini_imagenet('~/Downloads')
+ds_train, ds_novel, ds_test = mini_imagenet("~/Downloads")
 ds_train, ds_base = split_dataset(ds_train, frac=0.95, even_class_dist=True)
 
 pt_args = PretrainArguments(
-    modeldir='~/Downloads/models/',
-    batch_size=8,
+    modeldir="~/Downloads/models/",
+    logging_steps=250,
+    batch_size=64,
     gradient_accumulation_steps=1,
     num_epochs=100,
+    num_workers=10,
     evaluation_strategy=EvaluationStrategy.EPOCH,
-    metric_for_best_model='eval-base-class-loss',
+    scheduler_update_strategy=SchedulerUpdateStrategy.EPOCH,
+    metric_for_best_model="eval-base-class-loss",
+    modeldir_prefix="pretraining",
 )
 
 fs_args = FewshotArguments(
-    modeldir='~/Downloads/models/',
-    nways=2,
-    ksupport=1,
-    kquery=1,
-    batch_size=2,
+    modeldir="~/Downloads/models/",
+    nways=5,
+    ksupport=10,
+    kquery=15,
+    batch_size=5,
     num_epochs=100,
     epoch_steps=100,
     eval_steps=10,
     num_workers=10,
+    logging_steps=250,
     gradient_accumulation_steps=1,
-    metric_for_best_model='eval-novel-loss',
+    metric_for_best_model="eval-novel-loss",
     evaluation_strategy=EvaluationStrategy.STEPS,
 )
 
 eval_taskgen = EvalTaskGenerator(fs_args)
-eval_taskgen.add('base', ds_base)
-eval_taskgen.add('novel', ds_novel)
-eval_taskgen.add('base-class', ds_base, pt_args)
+eval_taskgen.add("base", ds_base)
+eval_taskgen.add("novel", ds_novel)
+eval_taskgen.add("base-class", ds_base, pt_args)
 
 
 def lr_update(epoch):
@@ -61,7 +68,7 @@ def lr_update(epoch):
 
 # Init baseline
 meta_baseline.init_pretraining(640, 64)
-optimizer = optim.SGD(meta_baseline.parameters(), lr=0.1, momentum=0.9)
+optimizer = optim.SGD(meta_baseline.parameters(), lr=0.01, momentum=0.9)
 lr_scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_update)
 pre_trainer = PreTrainer(
     meta_baseline,
@@ -74,6 +81,9 @@ pre_trainer.train()
 
 # Perform fewshot training
 fs_trainer = FewshotTrainer(
-    meta_baseline, fs_args, ds_train, eval_taskgen,
+    meta_baseline,
+    fs_args,
+    ds_train,
+    eval_taskgen,
 )
 fs_trainer.train()
