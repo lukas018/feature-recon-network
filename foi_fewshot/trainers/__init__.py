c019cee2f67aa1f4f5f1657d3b552a1932faa0f1
Lukas Lundmark <lukas.lundmark@foi.se>
2021-01-26 23:10:44 +0100

    Added tools for creating val and test generators
    Added some helper code for generating a common validation and common
    test task generator. This is to help reduce code repetition.

--- foi_fewshot/trainers/__init__.py
+++ foi_fewshot/trainers/__init__.py
@@ -3,6 +3,8 @@ from .trainer_arguments import FewshotArguments
 from .trainer_arguments import PretrainArguments
 from .trainer_arguments import SchedulerUpdateStrategy
 from .trainer_arguments import TrainingArguments
+from .trainer_utils import create_eval_taskgen
+from .trainer_utils import create_test_taskgen
 from .trainer_utils import EvalTaskGenerator
 from .trainer_utils import MetabatchWrapper
 from .trainers import FewshotTrainer
@@ -16,6 +18,8 @@ __all__ = [
     "SchedulerUpdateStrategy",
     "FewshotArguments",
     "TrainingArguments",
+    "create_eval_taskgen",
+    "create_test_taskgen",
     "PretrainArguments",
     "EvalTaskGenerator",
     "MetabatchWrapper",
--- foi_fewshot/trainers/trainer_utils.py
+++ foi_fewshot/trainers/trainer_utils.py
@@ -1,3 +1,4 @@
+from dataclasses import asdict
 from dataclasses import dataclass
 from typing import Dict
 from typing import List
@@ -174,3 +175,68 @@ class MetabatchWrapper(nn.Module):
             return collate.default_collate(outputs)
         else:
             return multi_size_collate(outputs)
+
+
+def create_eval_taskgen(ds_base, ds_novel, n_samples=25, batch_size=4):
+    """Helper function for creating a common evalution task generator
+
+    This task generator will create five different tasks.
+    Two nway=5, kshot=5, and two nway=5, kshot=1 for each datset and
+    a classification task on the base dataset
+
+    :param ds_base: Datset of baseclasses used to train the model
+    :param ds_novel: Dataset of novel (unseen) classes
+    :param n_steps: Number of meta-batches to evaluate on
+    :param batch_size: Number of tasks in each meta-batch
+
+    :returns: EvalTaskGenerator that can be used for evlauation
+    """
+
+    fs_eval_n5k1 = FewshotArguments(
+        nways=5,
+        ksupport=1,
+        kquery=15,
+        batch_size=batch_size,
+        epoch_steps=n_samples,
+    )
+
+    _args = asdict(fs_eval_n5k1)
+    _args.update({"ksupport": 5})
+    fs_eval_n5k5 = FewshotArguments(**_args)
+
+    eval_taskgen = EvalTaskGenerator()
+    eval_taskgen.add("base:n=5,k=5", ds_base, fs_eval_n5k5)
+    eval_taskgen.add("novel:n=5,k=5", ds_novel, fs_eval_n5k5)
+    eval_taskgen.add("base:n=5,k=1", ds_base, fs_eval_n5k1)
+    eval_taskgen.add("novel:n=5,k=1", ds_novel, fs_eval_n5k1)
+    eval_taskgen.add("base-class", ds_base, None)
+    return eval_taskgen
+
+
+def create_test_taskgen(ds_test, n_samples=500, batch_size=5):
+    """Helper function for creating a common testing task generator
+
+    This task generator will create two different tasks.
+    One nway=5, kshot=5, and one nway=5, kshot=1.
+
+    :param ds_test: Dataset of novel (unseen) classes
+    :param n_steps: Number of meta-batches to evaluate on
+    :param batch_size: Number of tasks in each meta-batch
+
+    :returns: EvalTaskGenerator that can be used for evlauation
+    """
+    test_args_n5_k1 = FewshotArguments(
+        nways=5,
+        ksupport=1,
+        kquery=5,
+        epoch_steps=n_samples,
+        batch_size=batch_size,
+    )
+    _args = asdict(test_args_n5_k1)
+    _args.update({"ksupport": 5})
+    test_args_n5_k5 = FewshotArguments(**_args)
+
+    test_taskgen = EvalTaskGenerator()
+    test_taskgen.add("test:n=5,k=1", ds_test, test_args_n5_k1)
+    test_taskgen.add("test:n=5,k=5", ds_test, test_args_n5_k5)
+    return test_taskgen
