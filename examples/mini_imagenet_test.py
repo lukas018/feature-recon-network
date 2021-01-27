f1f357b95627df793e7bbd5cac8e163769cb2f54
Lukas Lundmark <lukas.lundmark@foi.se>
2021-01-27 16:11:36 +0100

    Finalized meta-baseline example
    The meta-baseline example now get almost identically to its paper counterpart.

--- examples/mini_imagenet_test.py
+++ examples/mini_imagenet_test.py
@@ -1,3 +1,5 @@
+import json
+
 import torch
 from torch import optim
 
@@ -37,6 +39,7 @@ optimizer = optim.SGD(
 
 
 def lr_update(epoch):
+    """Very simple learning rate multiplire"""
     return 0.1 if epoch >= 90 else 1.0
 
 
@@ -50,10 +53,14 @@ pt_args = PretrainArguments(
     num_epochs=100,
     num_workers=10,
     evaluation_strategy=EvaluationStrategy.EPOCH,
+    # Use this to call the scheduler at the end of every epoch
     scheduler_update_strategy=SchedulerUpdateStrategy.EPOCH,
-    metric_for_best_model="eval-base-class-loss",
+    # Chen et al. simply train for 100 epochs, so lets to the same
+    metric_for_best_model=None,
     modeldir_prefix="pretraining",
+    disable_gradient_during_eval=True,
 )
+
 eval_taskgen = create_eval_taskgen(ds_base, ds_novel, n_samples=25, batch_size=4)
 pre_trainer = PreTrainer(
     meta_baseline,
@@ -65,8 +72,14 @@ pre_trainer = PreTrainer(
 )
 
 pre_trainer.train()
+test_taskgen = create_test_taskgen(ds_test, n_samples=500, batch_size=5)
+metrics = pre_trainer.evaluate(test_taskgen)
+print("====================")
+print("Classifier baseline:")
+print(json.dumps(metrics, indent=4))
+print("====================")
 
-# Perform fewshot training
+# Perform fewshot training using nway=5, kshot=1
 fs_args = FewshotArguments(
     modeldir="~/Downloads/fewshot-models/",
     logdir="~/Downloads/fewshot-logs/",
@@ -74,25 +87,43 @@ fs_args = FewshotArguments(
     ksupport=1,
     kquery=15,
     batch_size=1,
-    num_epochs=10,
-    epoch_steps=250,
+    num_epochs=30,
+    epoch_steps=200,
     eval_steps=50,
     num_workers=10,
-    logging_steps=50,
     gradient_accumulation_steps=4,
-    evaluation_strategy=EvaluationStrategy.STEPS,
-    scheduler_update_strategy=SchedulerUpdateStrategy.STEPS,
+    evaluation_strategy=EvaluationStrategy.EPOCH,
     modeldir_prefix="fewshot-training",
+    # Use the performance on the novel dataset to select the best mode
+    metric_for_best_model="eval-novel:n=5,k=1-loss",
+    disable_gradient_during_eval=True,
 )
 
+optimizer = optim.SGD(
+    meta_baseline.parameters(),
+    lr=0.001,
+    momentum=0.9,
+)
+
+# Chen et al doesn't use data augmentation in the meta-learning step
+ds_train.transform = ds_test.transform
+ds_base.transform = ds_test.transform
+
+# Lets use more samples now to get a more precise measurement of the performance
+eval_taskgen = create_eval_taskgen(ds_base, ds_novel, n_samples=200, batch_size=4)
+test_taskgen = create_test_taskgen(ds_test, n_samples=500, batch_size=5)
 fs_trainer = FewshotTrainer(
     meta_baseline,
     fs_args,
     ds_train,
     eval_taskgen,
+    optimizers=(optimizer, None),
     callbacks=[WriterCallback()],
 )
+
 fs_trainer.train()
-test_taskgen = create_test_taskgen(ds_test, n_samples=500, batch_size=5)
 metrics = fs_trainer.evaluate(test_taskgen)
-print(metrics)
+print("====================")
+print("Meta-baseline:")
+print(json.dumps(metrics, indent=4))
+print("====================")
