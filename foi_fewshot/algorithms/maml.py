Small fixed to gradient based methods

foi_fewshot/algorithms/maml.py
@@ -17,6 +17,7 @@ class MAML(nn.Module):
         :param loss_fn: The loss function to used to update during the inner step
         """
 
+        super().__init__()
         self.model = l2l_maml(model, fast_lr)
         self.update_steps = update_steps
         self.loss_fn = loss_fn
@@ -32,13 +33,16 @@ class MAML(nn.Module):
         :returns: Updated maml model
         """
 
+        self.model.train()
         maml = self.model.clone()
 
         n = support.shape[0]
         k = support.shape[1]
-        labels = torch.tensor(np.arange(n)).unflatten(1).repeat((1, k))
+        labels = torch.tensor(np.arange(n)).unsqueeze(1).repeat((1, k)).flatten()
+        labels = labels.to(support.device)
+
         for _ in range(self.update_steps):
-            logits = F.softmax(maml(support.flatten(0, 1)))
+            logits = maml(support.flatten(0, 1))
             error = self.loss_fn(logits, labels)
             maml.adapt(error)
 
@@ -47,7 +51,7 @@ class MAML(nn.Module):
 
         return maml
 
-    def forward(self, query, support=None):
+    def forward(self, query, support=None, **kwargs):
         """Perform a single forward step
 
         :param query:
@@ -60,8 +64,10 @@ class MAML(nn.Module):
             maml = self.adapt(support)
 
         logits = maml(query)
-        logits = F.softmax(logits)
-        return logits
+        outputs = dict()
+        outputs["logits"] = logits
+
+        return outputs
 
 
 class ANIL(nn.Module):
@@ -82,6 +88,7 @@ class ANIL(nn.Module):
         :param update_steps: The number of update steps to perform during the inner step
         :param loss_fn: The loss function to used to update during the inner step
         """
+        super().__init__()
         self.feature_extractor = feature_extractor
         self.head = l2l_maml(head, fast_lr)
         self.update_steps = update_steps
@@ -94,10 +101,11 @@ class ANIL(nn.Module):
         n = support.shape[0]
         k = support.shape[1]
         labels = torch.tensor(np.arange(n)).unflatten(1).repeat((1, k))
+        labels = labels.to(support.device)
         support_features = self.feature_extractor(support.flatten(0, 1))
 
         for _ in range(self.update_steps):
-            logits = F.softmax(head(support_features))
+            logits = head(support_features)
             error = self.loss_fn(logits, labels)
             head.adapt(error)
 
@@ -106,12 +114,14 @@ class ANIL(nn.Module):
 
         return head
 
-    def forward(self, query, support=None, overwrite=False):
+    def forward(self, query, support=None, overwrite=False, **kwargs):
         head = self.head
 
         if support is not None:
             head = self.adapt(support)
 
         query_features = self.feature_extractor(query)
-        logits = F.softmax(head(query_features))
-        return logits
+        logits = head(query_features)
+        outputs = dict()
+        outputs["logits"] = logits
+        return outputs

foi_fewshot/trainers/trainer_utils.py
@@ -177,7 +177,13 @@ class MetabatchWrapper(nn.Module):
             return multi_size_collate(outputs)
 
 
-def create_eval_taskgen(ds_base, ds_novel, n_samples=25, batch_size=4):
+def create_eval_taskgen(
+    ds_base,
+    ds_novel,
+    n_samples=25,
+    batch_size=4,
+    classification_task=False,
+):
     """Helper function for creating a common evalution task generator
 
     This task generator will create five different tasks.
@@ -209,7 +215,10 @@ def create_eval_taskgen(ds_base, ds_novel, n_samples=25, batch_size=4):
     eval_taskgen.add("novel:n=5,k=5", ds_novel, fs_eval_n5k5)
     eval_taskgen.add("base:n=5,k=1", ds_base, fs_eval_n5k1)
     eval_taskgen.add("novel:n=5,k=1", ds_novel, fs_eval_n5k1)
-    eval_taskgen.add("base-class", ds_base, None)
+
+    if classification_task:
+        eval_taskgen.add("base-class", ds_base, None)
+
     return eval_taskgen
 
 

foi_fewshot/trainers/trainers.py
@@ -6,6 +6,7 @@ import itertools
 import json
 import logging
 import math
+from contextlib import contextmanager
 from pathlib import Path
 from typing import Dict
 
@@ -521,7 +522,18 @@ class FewshotTrainer:
         :returns: loss, logits, labels
         """
 
-        with torch.no_grad():
+        # TODO(Lukas) Make a less ugly hack
+        @contextmanager
+        def dummy_cm():
+            yield
+
+        def detach(output):
+            """Detach item from tensor so that cuda memory can be released"""
+            if isinstance(output, torch.Tensor):
+                return output.detach().cpu().numpy()
+            return [detach(x) for x in output]
+
+        with torch.no_grad() if self.args.disable_gradient_during_eval else dummy_cm():
             outputs = model(**inputs)
             labels = inputs["query_labels"]
             logits = outputs["logits"]
@@ -531,6 +543,11 @@ class FewshotTrainer:
             if "loss" in outputs:
                 for x, y in zip(loss, outputs["loss"]):
                     x += y
+
+            # Detach the values from its torch tensors such that we can free cuda memory
+            loss = detach(loss)
+            logits = detach(logits)
+            labels = detach(labels)
 
             return loss, logits, labels
 
@@ -545,12 +562,12 @@ class FewshotTrainer:
         """
 
         model = self._envelop_model(model, isinstance(dataloader.dataset, TaskDataset))
-        model.eval()
+        # model.eval()
 
         total_loss, total_logits, total_labels = [], [], []
         for step, batch in enumerate(dataloader):
             loss, logits, labels = self.prediction_step(model, batch)
-            if len(loss.size()) == 0:
+            if len(loss.shape) == 0:
                 total_loss.append(loss)
             else:
                 total_loss.extend(loss)
@@ -577,12 +594,14 @@ class FewshotTrainer:
         """
 
         def _acc(logits, labels):
-            logits = logits.cpu()
-            labels = labels.long().cpu()
+            # logits = logits.cpu()
+            # labels = labels.long().cpu()
+            idx = np.argmax(logits, axis=-1)
 
-            idx = torch.argmax(logits)
+            # idx = torch.argmax(logits)
             size = 1.0 if len(labels.shape) == 0 else labels.shape[0]
-            acc = ((idx == labels).float().sum() / size).detach().numpy()
+            # acc = ((idx == labels).float().sum() / size).detach().numpy()
+            acc = np.sum(idx == labels) / size
             return acc
 
         accs = list(itertools.starmap(_acc, zip(logits, labels)))